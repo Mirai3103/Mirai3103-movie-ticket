98af2c51d6090d5d1f02308b580dd3cb9ac30007
Mirai3103 <huuhoag1412@gmail.com>
2024-04-07 01:30:55 +0700

    Add carousel functionality to chi_tiet_phim page

---
+++ public/chi_tiet_phim/carousel.js
@@ -0,0 +1,31 @@
+var multipleItemCarousel = document.querySelector('#carouselFood');
+
+if (window.matchMedia("(min-width: 576px)").matches) {
+    var carsouel = new bootstrap.Carousel(multipleItemCarousel, {
+        interval: false
+    })
+
+    var carouseWidth = $('.carousel-inner')[0].scrollWidth;
+    var cardWidth = $('.carousel-item').width() + 80;
+    
+    var scrollPosition = 0;
+
+    $('.carousel-control-next').on('click', function() {
+        if (scrollPosition < (carouseWidth - (cardWidth * 4))) {
+            console.log(cardWidth);
+            console.log('next');
+            scrollPosition = scrollPosition + cardWidth;
+            $('.carousel-inner').animate({ scrollLeft: scrollPosition}, 600);
+        }
+    })
+
+    $('.carousel-control-prev').on('click', function () {
+        if (scrollPosition > 0) {
+            console.log('prev');
+            scrollPosition = scrollPosition - cardWidth;
+            $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600)
+        }
+    })
+} else {
+    $(multipleItemCarousel).addClass('slide');
+}
