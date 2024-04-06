35aaefa46bc140d88c998772963455fe85f62fc6
Mirai3103 <huuhoag1412@gmail.com>
2024-04-07 01:30:14 +0700

    Update tim-kiem.view.php with new JavaScript logic and dependencies

--- app/views/tim-kiem.view.php
+++ app/views/tim-kiem.view.php
@@ -1,1 +1,11 @@
 <?php
+title("Dday la homr");
+require ('partials/head.php');
+?>
+
+
+<script type="module">
+import queryString from 'https://cdn.jsdelivr.net/npm/query-string@9.0.0/+esm'
+window.queryString = queryString
+</script>
+<?php require ('partials/footer.php'); ?>
