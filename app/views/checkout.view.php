c34dd29da2db3a8f1a49c92c244f3ac1e7884e93
Mirai3103 <huuhoag1412@gmail.com>
2024-04-07 01:29:56 +0700

    Update checkout.view.php with new JavaScript logic and dependencies

--- app/views/checkout.view.php
+++ app/views/checkout.view.php
@@ -1,19 +1,69 @@
 <?php
 title("Tiến hành thanh toán");
-require('partials/head.php');
+require ('partials/head.php');
 script(("/public/js/validation.js"));
 ?>
 
 
 
+<script src="
+https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js
+"></script>
+<script>
+const tickets = <?= json_encode($tickets) ?>;
+// ticked grouped by type
+const ticketGroups = _.groupBy(tickets, 'MaLoaiVe');
+</script>
 
 
-<main class="2xl:tw-max-w-7xl xl:tw-max-w-[78rem] lg:tw-max-w-[63rem]  tw-mx-auto tw-mt-10 md:tw-max-w-2xl sm:tw-max-w-[38rem]  tw-px-4 sm:tw-px-1" x-data="{
+
+<main
+    class="2xl:tw-max-w-7xl xl:tw-max-w-[78rem] lg:tw-max-w-[63rem]  tw-mx-auto tw-mt-10 md:tw-max-w-2xl sm:tw-max-w-[38rem]  tw-px-4 sm:tw-px-1"
+    x-data="{
     step: 1,
-}">
+    remainingTime: 0,
+    getRemainingDisplayTime() {
+        const minutes = Math.floor(this.remainingTime / 60);
+        const seconds = this.remainingTime % 60;
+        return `${minutes<10?'0'+minutes:minutes}:${seconds<10?'0'+seconds:seconds}`;
+    }
+}" x-init="
+    const lockToDayjs = dayjs.unix(<?= $bookingData['lockTo'] ?>);  
+    const now = dayjs();
+    const diff = lockToDayjs.diff(now, 'second');
+    console.log(diff);
+    remainingTime = diff;
+  const interval=  setInterval(() => {
+        remainingTime--;
+        if (remainingTime <= 0) {
+            clearInterval(interval);
+            window.timeout_modal.showModal();
+        }
+    }, 1000);
+">
+    <dialog id="timeout_modal" class="modal">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">
+                Thời gian giữ vé đã hết
+            </h3>
+            <p class="py-4">
+                Thời gian giữ vé đã hết, vui lòng chọn lại ghế
+            </p>
+            <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button onclick="window.location.href = '/phim/<?= $show['MaPhim'] ?>'" class="btn">
+                        Quay lại
+                    </button>
+                </form>
+            </div>
+        </div>
+    </dialog>
     <div class='tw-flex  tw-gap-x-4 tw-mb-8 '>
         <template x-for="i in [1, 2, 3]" :key="i">
-            <span x-on:click="if (i < step) { step = i }" x-bind:class="step >= i ? 'tw-bg-[#FFE7AA]' : ''" class="tw-w-12 tw-font-bold tw-text-2xl tw-h-12 tw-flex tw-items-center tw-justify-center tw-rounded-full  tw-border tw-border-black" x-text="i">
+            <span x-on:click="if (i < step) { step = i }" x-bind:class="step >= i ? 'tw-bg-[#FFE7AA]' : ''"
+                class="tw-w-12 tw-font-bold tw-text-2xl tw-h-12 tw-flex tw-items-center tw-justify-center tw-rounded-full  tw-border tw-border-black"
+                x-text="i">
             </span>
         </template>
     </div>
@@ -38,11 +88,11 @@ script(("/public/js/validation.js"));
         }
     })">
         <?php
-        include_once('partials/checkout.step2.php');
+        include_once ('partials/checkout.step2.php');
         ?>
-        <?php include_once('partials/checkout.step1.php'); ?>
+        <?php include_once ('partials/checkout.step1.php'); ?>
 
     </div>
 </main>
 
-<?php require('partials/footer.php'); ?>
+<?php require ('partials/footer.php'); ?>
