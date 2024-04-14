257f20989d4d20e701b5c5ffbe0debaf3f700981
Mirai3103 <huuhoag1412@gmail.com>
2024-04-14 23:22:01 +0700

    Fix Preload.php to include PromotionController

--- core/Preload.php
+++ core/Preload.php
@@ -17,7 +17,7 @@ Router::load_from_class(CinemaController::class);
 Router::load_from_class(SeatController::class);
 Router::load_from_class(StatusController::class);
 Router::load_from_class(MovieController::class);
-// Router::load_from_class(PromotionController::class);
+Router::load_from_class(PromotionController::class);
 Router::load_from_class(RoleController::class);
 Router::load_from_class(TicketTypeController::class);
 Router::build();
