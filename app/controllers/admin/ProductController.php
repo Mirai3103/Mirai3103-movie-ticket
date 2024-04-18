16ed76b4fea749feb9ef31089f8a966ab580f0a6
Mirai3103 <huuhoag1412@gmail.com>
2024-04-18 20:09:57 +0700

    Refactor ProductController to add new routes and services

--- app/controllers/admin/ProductController.php
+++ app/controllers/admin/ProductController.php
@@ -3,13 +3,27 @@
 use App\Models\JsonDataErrorRespose;
 use App\Models\JsonResponse;
 use App\Services\CinemaService;
+use App\Services\ComboService;
 use App\Services\RoomService;
+use App\Services\StatusService;
 use Core\Attributes\Controller;
 use Core\Attributes\Route;
 
-class ProductController {
-    #[Route("/admin/san-pham","GET")]
-    public static function abc() {
+class ProductController
+{
+    #[Route("/admin/san-pham", "GET")]
+    public static function index()
+    {
+
         return view("admin/san-pham/index");
     }
+
+    #[Route("/ajax/san-pham", "GET")]
+    public static function getProducts()
+    {
+        $products = ComboService::getAllFoodnDrink($_GET);
+        $statuses = StatusService::getAllStatus('ThucPham');
+        return ajax("admin/san-pham/table-row", ['products' => $products, 'statuses' => $statuses]);    
+    }
+
 }
