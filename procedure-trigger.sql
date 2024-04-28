45834d02f9515f18fadf6a322d3578b3dbf37905
Mirai3103 <huuhoag1412@gmail.com>
2024-04-28 23:16:41 +0700

    Refactor database triggers and procedures for updating CT_HoaDon_Combo and CT_HoaDon_ThucPham tables

--- procedure-trigger.sql
+++ procedure-trigger.sql
@@ -133,3 +133,142 @@ END
 END$$ 
 
 DELIMITER;
+
+DELIMITER $$
+
+CREATE TRIGGER update_ct_hoadon_combo_thanhtien BEFORE 
+INSERT ON CT_HoaDon_Combo FOR EACH ROW 
+BEGIN 
+DECLARE
+	gia_combo INT;
+	SELECT GiaCombo INTO gia_combo
+	FROM Combo
+	WHERE
+	    MaCombo = NEW.MaCombo;
+	SET NEW.ThanhTien = gia_combo * NEW.SoLuong;
+END$$ 
+
+DELIMITER;
+
+DELIMITER $$
+
+CREATE TRIGGER update_ct_hoadon_combo_thanhtien_after_update 
+AFTER UPDATE ON CT_HoaDon_Combo FOR EACH ROW 
+BEGIN 
+DECLARE
+	gia_combo INT;
+	SELECT GiaCombo INTO gia_combo
+	FROM Combo
+	WHERE
+	    MaCombo = NEW.MaCombo;
+	SET NEW.ThanhTien = gia_combo * NEW.SoLuong;
+END$$ 
+
+DELIMITER;
+
+DELIMITER $$
+
+CREATE TRIGGER update_ct_hoadon_thucpham_thanhtien 
+BEFORE INSERT ON CT_HoaDon_ThucPham FOR EACH ROW 
+BEGIN 
+DECLARE
+	gia_thucpham INT;
+	SELECT GiaThucPham INTO gia_thucpham
+	FROM ThucPham
+	WHERE
+	    MaThucPham = NEW.MaThucPham;
+	SET NEW.ThanhTien = gia_thucpham * NEW.SoLuong;
+END$$ 
+
+DELIMITER;
+
+DELIMITER $$
+
+CREATE TRIGGER update_ct_hoadon_thucpham_thanhtien_after_update 
+AFTER UPDATE ON CT_HoaDon_ThucPham FOR EACH ROW 
+BEGIN 
+DECLARE
+	gia_thucpham INT;
+	SELECT GiaThucPham INTO gia_thucpham
+	FROM ThucPham
+	WHERE
+	    MaThucPham = NEW.MaThucPham;
+	SET NEW.ThanhTien = gia_thucpham * NEW.SoLuong;
+END$$ 
+
+DELIMITER;
+
+DELIMITER $$
+
+CREATE PROCEDURE update_thanhtien_ct_hoadon() 
+BEGIN 
+	-- Cập nhật thành tiền cho CT_HoaDon_Combo
+DECLARE
+	done INT DEFAULT FALSE;
+DECLARE
+	ma_hoa_don_combo CHAR(36);
+DECLARE
+	ma_combo_combo INT;
+DECLARE
+	so_luong_combo INT;
+DECLARE
+	gia_combo INT;
+DECLARE
+	ma_hoa_don_thucpham CHAR(36);
+DECLARE
+	ma_thucpham INT;
+DECLARE
+	so_luong_thucpham INT;
+DECLARE
+	gia_thucpham INT;
+DECLARE
+	cur_thucpham CURSOR FOR
+	SELECT MaHoaDon, MaThucPham, SoLuong
+	FROM CT_HoaDon_ThucPham;
+DECLARE
+	cur_combo CURSOR FOR
+	SELECT MaHoaDon, MaCombo, SoLuong
+	FROM CT_HoaDon_Combo;
+DECLARE
+	CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+	OPEN cur_combo;
+	 read_loop : LOOP FETCH cur_combo INTO ma_hoa_don_combo , ma_combo_combo , so_luong_combo;
+	IF done THEN LEAVE read_loop;
+END
+	IF;
+	SELECT GiaCombo INTO gia_combo
+	FROM Combo
+	WHERE
+	    MaCombo = ma_combo_combo;
+	UPDATE CT_HoaDon_Combo
+	SET
+	    ThanhTien = gia_combo * so_luong_combo
+	WHERE
+	    MaHoaDon = ma_hoa_don_combo
+	    AND MaCombo = ma_combo_combo;
+END
+	LOOP read_loop;
+	CLOSE cur_combo;
+	-- Cập nhật thành tiền cho CT_HoaDon_ThucPham
+	SET done = FALSE;
+	OPEN cur_thucpham;
+	 read_loop2 : LOOP FETCH cur_thucpham INTO ma_hoa_don_thucpham , ma_thucpham , so_luong_thucpham;
+	IF done THEN LEAVE read_loop2;
+END
+	IF;
+	SELECT GiaThucPham INTO gia_thucpham
+	FROM ThucPham
+	WHERE
+	    MaThucPham = ma_thucpham;
+	UPDATE CT_HoaDon_ThucPham
+	SET
+	    ThanhTien = gia_thucpham * so_luong_thucpham
+	WHERE
+	    MaHoaDon = ma_hoa_don_thucpham
+	    AND MaThucPham = ma_thucpham;
+END
+	LOOP read_loop2;
+	CLOSE cur_thucpham;
+END$$ 
+
+DELIMITER;
