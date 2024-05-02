a23ab6541b37effa4ea784973cffacad7e6da904
luongminhthevinh <luongminhthevinh1606@outlook.com>
2024-05-02 16:21:26 +0700

    loi 403 & 404

---
+++ app/views/loi/404.view.php
@@ -0,0 +1,40 @@
+<?php
+title("Lỗi 404");
+?>
+<div class="container-fluid p-0">
+    <div class="row">
+        <div class="col-12">
+            <div class="container-fluid p-0 mt-5">
+                <div class="row text-center">
+                    <p class="fw-bold m-0" style="font-size: 200px;">404</p>
+                </div>
+
+                <div class="row text-center mb-4">
+                    <h3 class="fs-1 fw-bold">Xin lỗi, không tìm thấy
+                        trang!</h3>
+                </div>
+
+                <div class="row text-center mb-5">
+                    <p class="fw-medium text-body-tertiary m-0">Xin lỗi,
+                        chúng tôi không thể tìm thấy trang bạn đang tìm
+                        kiếm. <br>
+                        Có lẽ bạn đã gõ nhầm URL? Hãy chắc chắn để kiểm
+                        tra chính tả.</p>
+                </div>
+
+                <div class="row text-center">
+                    <form>
+                        <button type="submit" class="btn btn-dark fw-medium me-4">Về trang
+                            chủ</button>
+
+                        <button type="submit" class="btn btn-dark fw-medium">Quay
+                            lại</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+?>
